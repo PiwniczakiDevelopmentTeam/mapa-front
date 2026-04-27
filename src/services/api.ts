eb213aa8cbd1b218c567a0eeb6d47e6f1edc0c1e
AFvTD <alfreddzek123@gmail.com>
2026-04-27 16:28:03 +0200

    fix: api base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useUserStore } from "@/stores/userStore";
 
 const api = axios.create({
-  baseURL: "",
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? "",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
